51fd11fe5ea9b4e95926ccf691e29e53e409eedf
Baitur <omorovbaitur@gmail.com>
2024-12-15 16:11:21 +0600

    Added class BankCard

---
+++ src/model/BankCard.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Scanner;
+
+public class BankCard {
+    private String cardNumber;
+    private int onePassword;
+    private int replenishment;
+
+    public int getOnePassword() {
+        return onePassword;
+    }
+
+    public int getReplenishment() {
+        return replenishment;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setOnePassword(int onePassword) {
+        this.onePassword = onePassword;
+    }
+
+    public void setReplenishment(int replenishment) {
+        this.replenishment = replenishment;
+    }
+
+
+
+
+
+
+
+
+
+
+}
